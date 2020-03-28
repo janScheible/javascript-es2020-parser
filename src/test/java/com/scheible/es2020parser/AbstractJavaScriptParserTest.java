a3330bccc5109e3e5f37cb57f4998d61fcb03df2
Jan Scheible <janScheible@users.noreply.github.com>
2020-03-28 22:18:09 +0100

    - InputStream#readAllBytes() requires Java >= 9 --> custom implementation

--- src/test/java/com/scheible/es2020parser/AbstractJavaScriptParserTest.java
+++ src/test/java/com/scheible/es2020parser/AbstractJavaScriptParserTest.java
@@ -1,7 +1,10 @@
 package com.scheible.es2020parser;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.UncheckedIOException;
+import java.nio.charset.StandardCharsets;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 
@@ -11,22 +14,36 @@ import org.antlr.v4.runtime.CommonTokenStream;
  */
 public abstract class AbstractJavaScriptParserTest {
 
-	protected JavaScriptParser createParser(final Class<?> testClass) {
-		try {
-			final String javaScriptName = testClass.getSimpleName()
-					.substring(0, testClass.getSimpleName().length() - "Test".length()) + ".js";
-			final String javaScriptSource = new String(getClass().getResourceAsStream(javaScriptName).readAllBytes());
+	private static final int INPUT_BUFFER_LENGTH = 8192;
 
+	protected JavaScriptParser createParser(final Class<?> testClass) {
+		final String javaScriptName = testClass.getSimpleName()
+				.substring(0, testClass.getSimpleName().length() - "Test".length()) + ".js";
+
+		try (InputStream inputStream = getClass().getResourceAsStream(javaScriptName)) {
+			final String javaScriptSource = new String(readAllBytes(inputStream), StandardCharsets.UTF_8);
 			return createParser(javaScriptSource);
 		} catch (IOException ex) {
 			throw new UncheckedIOException(ex);
 		}
 	}
 
+	private static byte[] readAllBytes(final InputStream inputStream) throws IOException {
+		final byte[] buffer = new byte[INPUT_BUFFER_LENGTH];
+		final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+		int numberOfReadBytes;
+
+		while ((numberOfReadBytes = inputStream.read(buffer, 0, INPUT_BUFFER_LENGTH)) != -1) {
+			outputStream.write(buffer, 0, numberOfReadBytes);
+		}
+
+		return outputStream.toByteArray();
+	}
+
 	protected JavaScriptParser createParser(final String javaScriptSource) {
 		final JavaScriptLexer lexer = new JavaScriptLexer(CharStreams.fromString(javaScriptSource));
 		final JavaScriptParser parser = new JavaScriptParser(new CommonTokenStream(lexer));
-		
+
 		return parser;
 	}
 }
